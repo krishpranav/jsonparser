bb5f05e5de08f4cc8da1ff0612eed2bec9047961
krishpranav <krisna.pranav@gmail.com>
2021-12-29 16:04:12 +0530

    decoder: type

--- decoder.go
+++ decoder.go
@@ -21,4 +21,22 @@ type MetaValue struct {
 }
 
 type KV struct {
+	Key   string      `json:"key"`
+	Value interface{} `json:"value"`
+}
+
+type KVS []KV
+
+type Decoder struct {
+	*scanner
+	emitDepth     int
+	emitKV        bool
+	emitRecursive bool
+	objectAsKVS   bool
+	depth         int
+	scratch       *scratch
+	metaCh        chan *MetaValue
+	err           error
+	lineNo        int
+	lineStart     int64
 }
