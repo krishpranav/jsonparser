53878b63bf9f27a55aed10c420f7ba2d7fc370f5
krishpranav <krisna.pranav@gmail.com>
2021-12-29 16:13:37 +0530

    cmd: jsonparser

--- cmd/jsonparser/jsonparser.go
+++ cmd/jsonparser/jsonparser.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
-func main() {
-	var version string
-	version = "JsonParser: 1.0.0"
+var (
+	depthFlag   = flag.Int("d", 0, "")
+	kvFlag      = flag.Bool("kv", false, "")
+	verboseFlag = flag.Bool("v", false, "")
+	helpFlag    = flag.Bool("h", false, "")
+)
 
-	fmt.Println(version)
+func exitErr(err error) {
+	fmt.Fprintf(os.Stderr, "[\033[31merror\033[0m] %s", err)
+	os.Exit(1)
+}
+
+var Msg = `jsonparse - a golang json parsing tool
+
+using: jsonpars [options]
+
+options:
+
+`
+
+func help() {
+	fmt.Println(Msg)
 }
