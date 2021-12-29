89bb713236b3f408d3f6831985cdbfa28269d8a5
krishpranav <krisna.pranav@gmail.com>
2021-12-29 16:16:15 +0530

    cmd: jsonparser printVal

--- cmd/jsonparser/jsonparser.go
+++ cmd/jsonparser/jsonparser.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
+
+	"github.com/krishpranav/jsonparser"
 )
 
 var (
@@ -12,6 +15,40 @@ var (
 	verboseFlag = flag.Bool("v", false, "")
 	helpFlag    = flag.Bool("h", false, "")
 )
+
+func printVal(mv *jsonparser.MetaValue) {
+	b, err := json.Marshal(mv.Value)
+	if err != nil {
+		exitErr(err)
+	}
+
+	s := string(b)
+	var label string
+
+	switch mv.Value.(type) {
+	case []interface{}:
+		label = "array "
+	case float64:
+		label = "float "
+	case jsonparser.KV:
+		label = "kv "
+	case string:
+		label = "string "
+	case map[string]interface{}:
+		label = "object"
+	}
+
+	if *verboseFlag {
+		end := mv.Offset + mv.Length
+		fmt.Printf("%d\t%03d\t%03d\t%s| %s\n", mv.Depth, mv.Offset, end, label, s)
+		return
+	}
+	fmt.Println(s)
+}
+
+func main() {
+
+}
 
 func exitErr(err error) {
 	fmt.Fprintf(os.Stderr, "[\033[31merror\033[0m] %s", err)
