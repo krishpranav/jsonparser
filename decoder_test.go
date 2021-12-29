3f226b8b4074dc037cf072168ee817b8e4dc8c19
krishpranav <krisna.pranav@gmail.com>
2021-12-29 16:43:15 +0530

    decoder_test: TestDecoderFlat

--- decoder_test.go
+++ decoder_test.go
@@ -27,3 +27,107 @@ func TestDecoderSimple(t *testing.T) {
 		t.Fatalf("decoder error: %s", err)
 	}
 }
+
+func TestDecoderNested(t *testing.T) {
+	var (
+		counter int
+		mv      *MetaValue
+		body    = `{
+  "1": {
+    "bio": "bada bing bada boom",
+    "id": 0,
+    "name": "Roberto",
+    "nested1": {
+      "bio": "utf16 surrogate (\ud834\udcb2)\n\u201cutf 8\u201d",
+      "id": 1.5,
+      "name": "Roberto*Maestro",
+      "nested2": { "nested2arr": [0,1,2], "nested3": {
+        "nested4": { "depth": "recursion" }}
+			}
+		}
+  },
+  "2": {
+    "nullfield": null,
+    "id": -2
+  }
+}`
+	)
+
+	decoder := NewDecoder(mkReader(body), 2)
+
+	for mv = range decoder.Stream() {
+		counter++
+		t.Logf("depth=%d offset=%d len=%d (%v)", mv.Depth, mv.Offset, mv.Length, mv.Value)
+	}
+
+	if err := decoder.Err(); err != nil {
+		t.Fatalf("decoder error: %s", err)
+	}
+}
+
+func TestDecoderFlat(t *testing.T) {
+	var (
+		counter int
+		mv      *MetaValue
+		body    = `[
+  "1st test string",
+  "Roberto*Maestro", "Charles",
+  0, null, false,
+  1, 2.5
+]`
+		expected = []struct {
+			Value     interface{}
+			ValueType ValueType
+		}{
+			{
+				"1st test string",
+				String,
+			},
+			{
+				"Roberto*Maestro",
+				String,
+			},
+			{
+				"Charles",
+				String,
+			},
+			{
+				0.0,
+				Number,
+			},
+			{
+				nil,
+				Null,
+			},
+			{
+				false,
+				Boolean,
+			},
+			{
+				1.0,
+				Number,
+			},
+			{
+				2.5,
+				Number,
+			},
+		}
+	)
+
+	decoder := NewDecoder(mkReader(body), 1)
+
+	for mv = range decoder.Stream() {
+		if mv.Value != expected[counter].Value {
+			t.Fatalf("got %v, expected: %v", mv.Value, expected[counter])
+		}
+		if mv.ValueType != expected[counter].ValueType {
+			t.Fatalf("got %v value type, expected: %v value type", mv.ValueType, expected[counter].ValueType)
+		}
+		counter++
+		t.Logf("depth=%d offset=%d len=%d (%v)", mv.Depth, mv.Offset, mv.Length, mv.Value)
+	}
+
+	if err := decoder.Err(); err != nil {
+		t.Fatalf("decoder error: %s", err)
+	}
+}
